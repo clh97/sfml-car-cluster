1bde9432eee9bad51fcce3e393e7f30b5d92192a
Michel Calheiros <michel@calheiros.dev>
2023-02-13 21:40:01 -0300

    Added sinusoidalInterpolate

--- src/effects/interpolate.hpp
+++ src/effects/interpolate.hpp
@@ -8,4 +8,17 @@ public:
   {
     return start + (end - start) * percent;
   }
+
+  static float sinusoidalInterpolate(float start, float end, float t)
+  {
+    float range = end - start;
+    float value = range * (std::sin(t * PI) + 1.f) / 2.f + start;
+    return value;
+  }
+
+  static float mapValue(float value, float inMin, float inMax, float outMin, float outMax)
+  {
+    float result = (value - inMin) / (inMax - inMin) * (outMax - outMin) + outMin;
+    return result;
+  }
 };
