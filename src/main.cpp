46cdf2b8fd9cb546743ce4e71a1cfed59dde8fbe
Michel Calheiros <michel@calheiros.dev>
2023-02-25 04:08:15 -0300

    Added ImGui integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cmath>
 #include <random>
-#include <imgui/imgui.h>
-#include <imgui/imgui-SFML.h>
-#include <imgui/backends/imgui_impl_opengl3.h>
+
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+#include "imgui-sfml/imgui-SFML.cpp"
 
 #include "font.hpp"
 #include "splash/splash.cpp"
@@ -18,9 +20,7 @@ public:
     {
         this->window.create(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE, WINDOW_STYLE);
 
-        ImGui::SFML::Init(this->window);
-
-        this->window.setFramerateLimit(APPLICATION_FPS);
+        this->window.setFramerateLimit(60);
 
         this->background.setSize(sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
         this->background.setOrigin(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
@@ -32,33 +32,36 @@ public:
         this->background_shader.setUniform("innerColor", sf::Glsl::Vec4(1.0f, 1.0f, 1.0f, 1.0f));
         this->background_shader.setUniform("outerColor", sf::Glsl::Vec4(0.0f, 0.0f, 0.0f, 1.0f));
 
-        this->deltaClock.restart();
+        int initResult = ImGui::SFML::Init(this->window);
+
+        if (initResult == 0)
+        {
+            cout << "ImGui::SFML::Init failed" << endl;
+        }
     }
 
     void run()
     {
         while (this->window.isOpen())
         {
-            this->timeSinceLastUpdate += this->deltaClock.restart();
-
             sf::Event event;
             while (this->window.pollEvent(event))
             {
-                ImGui::SFML::ProcessEvent(event);
+                ImGui::SFML::ProcessEvent(this->window, event);
                 if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
                     this->window.close();
             }
 
-            while (this->timeSinceLastUpdate > this->timePerFrame)
-            {
-                this->timeSinceLastUpdate -= this->timePerFrame;
-                ImGui::SFML::Update(this->window, this->timePerFrame);
-                this->update(this->timePerFrame);
-            }
+            sf::Time deltaTime = this->deltaClock.restart();
+
+            this->update(deltaTime);
+            ImGui::SFML::Update(this->window, deltaTime);
+
+            ImGui::Begin("Hello, world!");
+            ImGui::Button("Look at this pretty button");
+            ImGui::End();
 
             this->render(this->window);
-            ImGui::SFML::Render(this->window);
-
             this->window.display();
         }
 
@@ -70,8 +73,6 @@ private:
     sf::RectangleShape background;
     sf::Shader background_shader;
     sf::Clock deltaClock;
-    const sf::Time timePerFrame = sf::seconds(1.0f / APPLICATION_FPS);
-    sf::Time timeSinceLastUpdate = sf::Time::Zero;
 
     Splash splash = Splash();
     Cluster cluster = Cluster();
@@ -88,16 +89,18 @@ private:
 
         this->window.draw(this->background, &this->background_shader);
         this->cluster.draw(window);
+
+        ImGui::SFML::Render(this->window);
     }
 
     void update(sf::Time elapsed)
     {
-        this->window.setTitle(string(WINDOW_TITLE) + " | FPS: " + to_string(1.0f / elapsed.asSeconds()));
-        if (!this->splash.isDone())
-        {
-            this->splash.update();
-            return;
-        }
+        // this->window.setTitle(string(WINDOW_TITLE) + " | FPS: " + to_string(1.0f / elapsed.asSeconds()));
+        // if (!this->splash.isDone())
+        // {
+        //     this->splash.update();
+        //     return;
+        // }
 
         this->cluster.update(elapsed);
     };
