1e89d06697cd5d402bd23db69b39dfd4de2dd24d
Michel Calheiros <michel@calheiros.dev>
2023-02-27 11:12:03 -0300

    Adjusted compile.sh command

--- compile.sh
+++ compile.sh
@@ -4,4 +4,4 @@ cd /home/chimas/sfml-car-cluster
 
 # Configure and build the project
 cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S$(pwd) -B$(pwd)/build -G "Unix Makefiles"
-cmake --build $(pwd)/build --config Release --target InstrumentCluster -j $(nproc) --
+cmake --build $(pwd)/build --config Debug --target InstrumentCluster -j $(nproc) --
